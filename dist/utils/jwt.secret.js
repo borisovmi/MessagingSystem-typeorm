e862116f923a6e000b0dc29ccb463d762d493f58
borisovmi <michael.borisov10@gmail.com>
2020-12-07 12:57:14 +0200

    removed dist folder

--- dist/utils/jwt.secret.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.jwtSecret = void 0;
-exports.jwtSecret = "5EDA1C85D46CBA133CB042EB0D4E093A94DA6C467A3B55648CC992720DE649C1";
-// TODO: move this to .env file
-//# sourceMappingURL=jwt.secret.js.map
